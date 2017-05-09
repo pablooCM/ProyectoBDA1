db5ddf2bfb8965d1b3423959ddd546db2cbb7f22
Joel Gorin <joelmgr@gmail.com>
2017-05-09 09:57:15 -0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,104 @@
+// Express
+var express = require('express');
+var app = express();
+var router = express.Router();
+// Mongo
+var mongojs = require('mongojs');
+var db = mongojs('mean_stack_v3', ['student','courseList']);
+// JSON
+var bodyParser = require('body-parser');
+// Port
+var port = 8080;
+
+app.use(express.static( __dirname + "/modules"));
+app.use(bodyParser.json());
+
+
+
+/**********************
+	Login Functions
+**********************/
+app.get('/', function(req, res){
+	res.redirect('/');
+});
+
+app.get('/singup', function(req, res){
+	res.render('/singup/index.html');
+});
+
+/************************
+	Student Functions
+************************/
+app.get('/studentList', function(req, res){
+
+	db.studentList.find(function(err, docs){
+		console.log(docs);
+		res.json(docs);
+	});
+});
+
+app.post('/studentList', function(req,res){
+	db.studentList.insert(req.body, function(err, doc){
+		res.json(doc);
+	});
+});
+
+app.get('/studentList/:email', function(req, res){
+	var email = req.params.email;
+	db.studentList.findOne({email: email}, function(err, doc){
+		res.json(doc);
+	});
+});
+
+/************************
+	Courses Functions
+************************/
+app.get('/courseList', function(req, res){
+	db.courseList.find(function(err, docs){
+		res.json(docs);
+	});
+});
+
+app.post('/courseList', function (req, res){
+	db.courseList.insert(req.body, function(err, doc){
+		res.json(doc);
+	})
+});
+
+app.delete('/courseList/:id', function(req, res){
+	var id = req.params.id;
+	db.courseList.remove({_id: mongojs.ObjectId(id)}, function(err, doc){
+		res.json(doc);
+	})
+});
+
+app.get('/courseList/:id', function(req, res){
+	var id = req.params.id;
+	db.courseList.findOne({_id: mongojs.ObjectId(id)}, function(err, doc){
+		res.json(doc);
+	});
+});
+
+app.put('/courseList/:id', function(req, res){
+	var id = req.params.id;
+	db.courseList.findAndModify({query:{_id: mongojs.ObjectId(id)},
+		update:{$set:{codCourse: req.body.codCourse, courseName: req.body.courseName, semester: req.body.semester, year: req.body.year}},
+		new: true}, function(err, doc){
+			res.json(doc);
+		});
+});
+
+/************************
+	Courses Reports
+************************/
+app.get('/courseList/:courseName', function(req, res){
+	var courseName = req.params.courseName;
+	db.studentList.findOne({courseName: courseName}, function(err, doc){
+		res.json(doc);
+	});
+});
+
+
+
+app.listen(port);
+console.log("Server running on port: " + port);
