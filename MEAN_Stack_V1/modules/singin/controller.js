985cd429a4dc5461f4801735705ed91783d73b14
Joel Gorin <joelmgr@gmail.com>
2017-05-08 23:44:57 -0600

    Update controller.js

--- MEAN_Stack_V1/modules/singin/controller.js
+++ MEAN_Stack_V1/modules/singin/controller.js
@@ -1,17 +1,47 @@
-var app = angular.module('singin', []);
+var app = angular.module('singup', []);
+app.controller('SingupCtrl', ['$scope','$http', function($scope, $http) {
 
-app.controller('SinginCtrl',['$scope','$http', function($scope, $http){
-	var returnToLogin = function(){
-		$http.get("/student", function(){
-			console.log("Return to Login");
-			$scope.student = null;
+	var refresh = function() {
+		$http.get('/courseList').then( function(res) {
+			$scope.courseList = res.data;
+		});
+	}
+
+	refresh();
+
+	$scope.goLogin = function() {
+		$http.get('/').then( function(res) {
+			$scope.student = "";
 		});
 	}
 
 	$scope.addStudent = function(){
-		$http.post('/student', $scope.student).then(function(response){
-			console.log(response);
-			returnToLogin();
-		});	
+		if(	$scope.student == "" || 
+			$scope.student.name == "" || 
+			$scope.student.cardNumber == "" || 
+			$scope.student.email == "" || 
+			$scope.student.pass == "") {
+
+			window.alert("Rellene los datos solicitados");
+
+		} else {
+			$http.get('/studentList/' + $scope.student.email).then( function(response, err) {
+				if(!err){
+					if(response.data == null){
+						$http.post('/studentList', $scope.student).then( function(res, err2) {
+							if(!err2){
+								window.alert("Éxito en el registro del usuario");
+							} else {
+								window.alert("ERROR en la conexión");
+							}
+						});
+					} else {
+						window.alert("El correo electrónico que ingresó ya existe");
+					}
+				} else {
+					window.alert("ERROR en la conexión");
+				}
+			});
+		}
 	};
 }])
