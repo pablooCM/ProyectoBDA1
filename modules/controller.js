c74adee93db2058ab8e45ff2eff7f76e2a18d5e1
Joel Gorin <joelmgr@gmail.com>
2017-05-09 12:43:46 -0600

    Update controller.js

--- modules/controller.js
+++ modules/controller.js
@@ -1,28 +1,30 @@
 var app = angular.module('login', []);
-app.controller('LoginCtrl',['$scope','$http', function($scope, $http){
+app.controller('LoginCtrl',['$scope','$http','$window', function($scope, $http, $window){
+
+ 	$scope.goToPath = function(path) {
+ 		console.log("gotopath(" + path + ")");
+ 		$window.location.href = path;
+ 	}
 
 	$scope.login = function() {
 		if($scope.student.email == "admin" && $scope.student.pass == "pass"){
 			window.alert("Exito al entrar a su cuenta de administrador");
-			// Mostrar ventana de admin			
-		}
-		else{
+			$scope.goToPath("/home_manager/");
+
+		} else {
 			$http.get('/studentList/'+ $scope.student.email).then(function(response, err) {
 				if(!err){
 					if(response.data == null){
 						window.alert("Usuario o contraseña incorrecta");
-					}
-					else{
+					} else {
 						if ($scope.student.pass == response.data.pass){
 							window.alert("Exito al entrar a su cuenta");
-							// Mostrar ventana de estudiante
-						}
-						else{
+							$scope.goToPath("/home_student/");
+						} else {
 							window.alert("Usuario o contraseña incorrecta");
 						}
 					}
-				}
-				else{
+				} else {
 					window.alert("Error en la conexion");
 				}
 			});
