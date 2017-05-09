b765b75761f5d1312721e43321d763112117bbd6
Joel Gorin <joelmgr@gmail.com>
2017-05-09 00:05:15 -0600

    Update controller.js

--- MEAN_Stack_V1/modules/controller.js
+++ MEAN_Stack_V1/modules/controller.js
@@ -1,5 +1,30 @@
-var myApp = angular.module('myApp', []);
-myApp.controller('LoginCtrl',['$scope','$http', function($scope, $http){
-	
+var app = angular.module('login', []);
+app.controller('LoginCtrl',['$scope','$http', function($scope, $http){
 
+	$scope.login = function() {
+		if($scope.student.email == "admin" && $scope.student.pass == "pass"){
+			window.alert("Exito al entrar a su cuenta de administrador");
+			// Mostrar ventana de admin			
+		}
+		else{
+			$http.get('/studentList/'+ $scope.student.email).then(function(response, err) {
+				if(!err){
+					if(response.data == null){
+						window.alert("Favor revisar datos");
+					}
+					else{
+						if ($scope.student.pass == response.data.pass){
+							window.alert("Exito al entrar a su cuenta");
+						}
+						else{
+							window.alert("Contraseña incorrecta");
+						}
+					}
+				}
+				else{
+					window.alert("Error en la conexion");
+				}
+			});
+		}
+	}
 }])
